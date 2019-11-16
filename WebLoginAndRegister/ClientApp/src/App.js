fb2ef2240a15d43db69a88c90349453d8e06ea77
novakvova <novakvova@gmail.com>
2019-11-16 11:33:18 +0200

    Add url-link confirmemail

--- WebLoginAndRegister/ClientApp/src/App.js
+++ WebLoginAndRegister/ClientApp/src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
 import RegisterPage from './components/auth/Register';
+import ConfirmEmailPage from './components/auth/ConfirmEmail';
 
 export default () => (
   <Layout>
@@ -12,5 +13,6 @@ export default () => (
     <Route path='/counter' component={Counter} />
     <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
     <Route path='/register' component={RegisterPage} />
+    <Route path='/confirmemail' component={ConfirmEmailPage} />
   </Layout>
 );

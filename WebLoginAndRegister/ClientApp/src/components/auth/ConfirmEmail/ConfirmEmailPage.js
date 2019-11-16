0ef9b971a118d80db91ae260eb87bbf04be26689
novakvova <novakvova@gmail.com>
2019-11-16 11:58:40 +0200

    Add lib qs parse url

--- WebLoginAndRegister/ClientApp/src/components/auth/ConfirmEmail/ConfirmEmailPage.js
+++ WebLoginAndRegister/ClientApp/src/components/auth/ConfirmEmail/ConfirmEmailPage.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import qs from 'qs';
+
 class ConfirmEmailPage extends Component {
     state = { 
         code: ''
      }
     render() { 
+        console.log('----props----', this.props);
+        const {location} = this.props;
+        var result=qs.parse(location.search, { ignoreQueryPrefix: true });
+        console.log('-----parse url-----', result);
         return ( 
             <h1>Підтвердження пошти</h1>
+
          );
     }
 }
